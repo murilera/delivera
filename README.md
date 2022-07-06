fcbb56909741e5b76180ae71c2eb692634aadcad
Murilo Rodegheri <murilo.rodegheri@dowjones.com>
2022-07-06 18:51:55 -0300

    feat: add position simulator microservice

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
 # delivera
+
+delivery web app with position simulator built using microservices.
+
+## position simulatior
+
+- golang
+
+## backend
+
+- nest.js
+- mongodb
+
+## frontend
+
+- react
+
+## streaming data
+
+- kafka
+- kafka connect
+
+## visualize data
+
+- elasticsearch
+- kibana
+
+## deploy
+
+- docker
+- kubernetes
+
+## monitoring
+
+- istio
+- kiali
+- prometheus
+- grafana
---
+++ application/kafka/produce.go
@@ -0,0 +1,33 @@
+package kafka
+
+import (
+	"encoding/json"
+
+	"log"
+	"os"
+	"time"
+
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/murilera/delivera/application/route"
+	"github.com/murilera/delivera/infra/kafka"
+)
+
+// Produce is responsible to publish the positions of each request
+// Example of a json request:
+//{"clientId":"1","routeId":"1"}
+//{"clientId":"2","routeId":"2"}
+//{"clientId":"3","routeId":"3"}
+func Produce(msg *ckafka.Message) {
+	producer := kafka.NewKafkaProducer()
+	route := route.NewRoute()
+	json.Unmarshal(msg.Value, &route)
+	route.LoadPositions()
+	positions, err := route.ExportJsonPositions()
+	if err != nil {
+		log.Println(err.Error())
+	}
+	for _, p := range positions {
+		kafka.Publish(p, os.Getenv("KafkaProduceTopic"), producer)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
